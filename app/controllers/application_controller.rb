3aece83b889302cb9cb8c31c534531a8f111d49e
porzingod <jasonlim.ny@gmail.com>
2018-02-21 14:52:37 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,48 @@
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{Recipe.last.id}"
+  end
+
+  get '/recipes' do
     erb :index
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  #why does post work but not patch?
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    @recipe = recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    # binding.pry
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+    # binding.pry
+  end
+
+  delete '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    # erb :index
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,9 @@ ENV["SINATRA_ENV"] = "test"
 
 require_relative '../config/environment'
 require 'rack/test'
-require 'capybara/rspec'
+require 'capybara'
 require 'capybara/dsl'
+require 'capybara/rspec'
 
 if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
