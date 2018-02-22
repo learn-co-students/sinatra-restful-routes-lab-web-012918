9f1193211f082e01ca1b06285ed811e674fbbd08
zitro <bryan@bryanortiz.com>
2018-02-21 20:28:03 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,23 @@
 #Placeholder for a model
+class Recipe < ActiveRecord::Base
+
+end
+# class Recipe
+#
+# attr_accessor :name, :ingredients, :cook_time
+# @@all = []
+# def initialize(attributes)
+# 	@name = attributes[name]
+# 	@ingredients = attributes[ingredients]
+# 	@cook_time = attributes[cook_time]
+# 	@@all << self
+#
+# end
+#
+# def self.all
+# 		@@all
+# end
+#
+#
+#
+# end
